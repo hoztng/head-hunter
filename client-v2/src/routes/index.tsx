c98bb4acea83d65e4721a2df7bbbf6960da026ba
samadhi-allion <samadhip@alliontechnologies.com>
2024-01-18 08:26:37 +0530

    fix router path issue

--- client-v2/src/routes/index.tsx
+++ client-v2/src/routes/index.tsx
@@ -1,19 +1,21 @@
 // src/routes/PublicRoutes.ts
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '@views/HomePage';
 import AboutPage from '@views/AboutPage';
 import NotFoundPage from '@views/NotFoundPage';
 
 const PublicRoutes:React.FC = () => {
+  const basePath = import.meta.env.BASE_URL;
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/about" element={<AboutPage/>} />
-      {/* <PrivateRoute path="/dashboard" component={DashboardPage} /> */}
+      <Route path={`/${basePath}`} element={<HomePage/>} />
+      {/* <Route path="/head-hunter" element={<HomePage/>} /> */}
+      <Route path={`/${basePath}about`}  element={<AboutPage/>} />
       <Route path="*" element={<NotFoundPage/>} />
     </Routes>
   );
+
 };
 
 export default PublicRoutes;
